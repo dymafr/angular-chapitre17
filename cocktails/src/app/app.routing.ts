9a1ff3814718cc1bbb28d73685407beb516f55eb
Erwan Vallée <erwanp.vallee@gmail.com>
2020-04-21 16:58:54 +0200

    feat: maj ng9

--- cocktails/src/app/app.routing.ts
+++ cocktails/src/app/app.routing.ts
@@ -2,7 +2,7 @@ import { Route, RouterModule } from '@angular/router';
 
 const APP_ROUTE: Route[] = [
   { path: '', redirectTo: 'cocktails', pathMatch: 'full' },
-  { path: 'panier', loadChildren: 'app/panier/panier.module#PanierModule' },
+  { path: 'panier', loadChildren: () => import('app/panier/panier.module').then(m => m.PanierModule) },
 ];
 
 export const AppRouting = RouterModule.forRoot(APP_ROUTE);
