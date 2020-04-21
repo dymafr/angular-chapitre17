01cda7ec19aa8ea6041cf5461e1706d0a1aacf73
Erwan Vallée <erwanp.vallee@gmail.com>
2020-04-21 17:06:31 +0200

    fix: ts warning

--- cocktails/src/app/cocktail-container/cocktail-edit/cocktail-edit.component.ts
+++ cocktails/src/app/cocktail-container/cocktail-edit/cocktail-edit.component.ts
@@ -1,59 +1,72 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
-import { CocktailService } from '../../shared/services/cocktail.service';
-import { ActivatedRoute, Params } from '@angular/router';
-import { Cocktail } from '../../shared/models/cocktail.model';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators, FormArray } from "@angular/forms";
+import { CocktailService } from "../../shared/services/cocktail.service";
+import { ActivatedRoute, Params } from "@angular/router";
+import { Cocktail } from "../../shared/models/cocktail.model";
 
 @Component({
-  selector: 'app-cocktail-edit',
-  templateUrl: './cocktail-edit.component.html',
-  styleUrls: ['./cocktail-edit.component.css']
+  selector: "app-cocktail-edit",
+  templateUrl: "./cocktail-edit.component.html",
+  styleUrls: ["./cocktail-edit.component.css"],
 })
 export class CocktailEditComponent implements OnInit {
   public cocktailForm: FormGroup;
   public cocktail: Cocktail;
   private edit: boolean;
 
-  constructor(private activatedRoute: ActivatedRoute, private fb: FormBuilder, private cocktailService: CocktailService) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private fb: FormBuilder,
+    private cocktailService: CocktailService
+  ) {}
 
   ngOnInit() {
-    this.activatedRoute.params.subscribe(( params: Params ) => {
+    this.activatedRoute.params.subscribe((params: Params) => {
       if (params.index) {
         this.edit = true;
-        this.cocktailService.getCocktail(params.index).subscribe( (cocktail: Cocktail) => {
-          this.cocktail = cocktail;
-          this.initForm(this.cocktail);
-        });
+        this.cocktailService
+          .getCocktail(params.index)
+          .subscribe((cocktail: Cocktail) => {
+            this.cocktail = cocktail;
+            this.initForm(this.cocktail);
+          });
       } else {
         this.edit = false;
         this.initForm();
       }
-    })
+    });
   }
 
-  initForm(cocktail = { name: '', img: '', desc: '', ingredients: []}) {
+  initForm(cocktail = { name: "", img: "", desc: "", ingredients: [] }) {
     this.cocktailForm = this.fb.group({
       name: [cocktail.name, Validators.required],
       img: [cocktail.img, Validators.required],
       desc: [cocktail.desc],
-      ingredients: this.fb.array(cocktail.ingredients.map( ingredient => this.fb.group({ name: [ingredient.name], quantity: [ingredient.quantity]})))
-    })
+      ingredients: this.fb.array(
+        cocktail.ingredients.map((ingredient) =>
+          this.fb.group({
+            name: [ingredient.name],
+            quantity: [ingredient.quantity],
+          })
+        )
+      ),
+    });
   }
 
   addIngredient(): void {
-    (<FormArray>this.cocktailForm.get('ingredients')).push(this.fb.group({
-      name: [''],
-      quantity: ['']
-    }))
+    (this.cocktailForm.get("ingredients") as FormArray).push(
+      this.fb.group({
+        name: [""],
+        quantity: [""],
+      })
+    );
   }
 
   createCocktail() {
     if (this.edit) {
-      this.cocktailService.editCocktail(this.cocktailForm.value)
+      this.cocktailService.editCocktail(this.cocktailForm.value);
     } else {
       this.cocktailService.addCocktail(this.cocktailForm.value);
     }
   }
-
-
 }
--- cocktails/src/app/shared/services/cocktail.service.ts
+++ cocktails/src/app/shared/services/cocktail.service.ts
@@ -1,47 +1,62 @@
-import { Injectable } from '@angular/core';
-import { Cocktail } from '../models/cocktail.model';
-import {BehaviorSubject, Observable} from 'rxjs';
-import { Ingredient } from '../models/ingredient.model';
-import {HttpClient} from "@angular/common/http";
-import {filter, map} from "rxjs/operators";
+import { Injectable } from "@angular/core";
+import { Cocktail } from "../models/cocktail.model";
+import { BehaviorSubject, Observable } from "rxjs";
+import { Ingredient } from "../models/ingredient.model";
+import { HttpClient } from "@angular/common/http";
+import { filter, map } from "rxjs/operators";
 
 @Injectable()
 export class CocktailService {
-  public cocktails: BehaviorSubject<Cocktail[]> = new BehaviorSubject(null)
+  public cocktails: BehaviorSubject<Cocktail[]> = new BehaviorSubject(null);
 
   constructor(private http: HttpClient) {
     this.cocktailsInit();
   }
 
-  cocktailsInit(): void{
-    this.http.get('https://cocktails-63318.firebaseio.com/cocktails.json')
-      .subscribe( (cocktails: Cocktail[]) => {
+  cocktailsInit(): void {
+    this.http
+      .get("https://cocktails-63318.firebaseio.com/cocktails.json")
+      .subscribe((cocktails: Cocktail[]) => {
         this.cocktails.next(cocktails);
-      })
+      });
   }
 
   getCocktail(index: number): Observable<Cocktail> {
     return this.cocktails.pipe(
-            filter( cocktails => cocktails != null ),
-			      map( cocktails => cocktails[index]))
+      filter((cocktails) => cocktails != null),
+      map((cocktails) => cocktails[index])
+    );
   }
 
   addCocktail(cocktail: Cocktail): void {
     const cocktails = this.cocktails.value.slice();
-    cocktails.push(new Cocktail(cocktail.name, cocktail.img, cocktail.desc, cocktail.ingredients.map( ingredient => new Ingredient(ingredient.name, ingredient.quantity) ) ))
+    cocktails.push(
+      new Cocktail(
+        cocktail.name,
+        cocktail.img,
+        cocktail.desc,
+        cocktail.ingredients.map(
+          (ingredient) => new Ingredient(ingredient.name, ingredient.quantity)
+        )
+      )
+    );
     this.cocktails.next(cocktails);
   }
 
   editCocktail(editCocktail: Cocktail): void {
     const cocktails = this.cocktails.value.slice();
-    const index = cocktails.map( c => c.name ).indexOf(editCocktail.name);
+    const index = cocktails.map((c) => c.name).indexOf(editCocktail.name);
     cocktails[index] = editCocktail;
     this.cocktails.next(cocktails);
     this.save();
   }
 
-  save(): void{
-    this.http.put('https://cocktails-63318.firebaseio.com/cocktails.json', this.cocktails.value).subscribe( res => console.log(res));
+  save(): void {
+    this.http
+      .put(
+        "https://cocktails-63318.firebaseio.com/cocktails.json",
+        this.cocktails.value
+      )
+      .subscribe((res) => console.log(res));
   }
-
 }
